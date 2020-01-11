11b1d7d36235af9900c48cbb73deb95c398b9293
engikeer <engikeer@github.com>
2020-01-11 18:31:58 +0800

    update

---
+++ servlet-01/src/main/java/com/mfun/main/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.mfun.main;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        response.setContentType("text/html");
+        // 响应的写出器以该编码写出文本
+        // 告诉浏览器，响应里文本的编码
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+        out.println("<h3>你好，世界</h3>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
---
+++ servlet-01/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Servlet-01</title>
+    <%@ page contentType="text/html; charset=utf-8" pageEncoding="utf-8" %>
+</head>
+<body>
+<h2>首页：Hello World!</h2>
+</body>
+</html>
