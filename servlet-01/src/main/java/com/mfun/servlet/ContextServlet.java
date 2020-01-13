9da6118439082822746035e0b02ad8bc3e707873
engikeer <engikeer@github.com>
2020-01-13 18:39:53 +0800

    Update.

---
+++ servlet-01/src/main/java/com/mfun/servlet/ContextServlet.java
@@ -0,0 +1,41 @@
+package com.mfun.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.Properties;
+
+public class ContextServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        ServletContext context = getServletContext();
+        InputStream resource = context.getResourceAsStream("WEB-INF/classes/db.properties");
+//        InputStream resource = context.getResourceAsStream("/WEB-INF/classes/db.properties");
+        Properties prop = new Properties();
+        prop.load(resource);
+        String username = prop.getProperty("username");
+        String password = prop.getProperty("password");
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.println("<h3>User: " + username + "</h3>");
+            writer.println("<h3>Password: " + password + "</h3>");
+        }
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
