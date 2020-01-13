e4d7a69b526e600c87cd2b1daf6cde2deffeae95
engikeer <engikeer@github.com>
2020-01-13 16:23:23 +0800

    Update.

---
+++ servlet-01/src/main/java/com/mfun/servlet/ErrorServlet.java
@@ -0,0 +1,25 @@
+package com.mfun.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ErrorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.println("<h2>Error page.</h2>");
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
