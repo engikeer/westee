23cbed975127e62dcd7925f89a7ca298d7d7ac8e
engikeer <engikeer@github.com>
2020-01-16 22:01:02 +0800

    Update.

---
+++ servlet-01/src/test/java/com/mfun/servlet/ServletTest.java
@@ -0,0 +1,14 @@
+package com.mfun.servlet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ServletTest {
+
+    @Test
+    public void test() {
+        System.out.println("测试开始");
+        assertEquals(2, 1 + 1);
+    }
+}
