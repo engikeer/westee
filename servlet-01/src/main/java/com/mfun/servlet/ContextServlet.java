cd8b64fb1377169b618db2d059d51b6d7f65f732
engikeer <engikeer@github.com>
2020-01-17 08:13:01 +0800

    Update.

--- servlet-01/src/main/java/com/mfun/servlet/ContextServlet.java
+++ servlet-01/src/main/java/com/mfun/servlet/ContextServlet.java
@@ -5,11 +5,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.net.URL;
 import java.util.Properties;
 
 public class ContextServlet extends HttpServlet {
