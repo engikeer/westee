76c5a8d32e425f266f95b99f9213b0a3f424cbcb
engikeer <engikeer@github.com>
2020-01-18 01:13:33 +0800

    Update.

--- smbms/src/main/java/com/mfun/controller/user/LoginController.java
+++ smbms/src/main/java/com/mfun/controller/user/LoginController.java
@@ -19,22 +19,26 @@ public class LoginController extends HttpServlet {
         String userCode = req.getParameter("userCode");
         String password = req.getParameter("userPassword");
 
-        UserService userService = new UserServiceImpl();
-        try {
-            User user = userService.login(userCode, password);
-            if (user != null) {
-                // 登陆成功后将用户对象放入 session 中
-                req.getSession().setAttribute(ControllerEnum.USER_SESSION.getValue(), user);
-                System.out.println("登陆成功");
-                // 跳转到主页
-                resp.sendRedirect("jsp/frame.jsp");
-            } else {
-                // 没有对应的用户名和密码，返回登陆页
-                req.setAttribute("error", "用户名或密码不正确");
-                req.getRequestDispatcher("login.jsp").forward(req, resp);
+        if (userCode == null || password == null) {
+            resp.sendRedirect("/smbms");
+        } else {
+            UserService userService = new UserServiceImpl();
+            try {
+                User user = userService.login(userCode, password);
+                if (user != null) {
+                    // 登陆成功后将用户对象放入 session 中
+                    req.getSession().setAttribute(ControllerEnum.USER_SESSION.getValue(), user);
+                    System.out.println("登陆成功");
+                    // 跳转到主页
+                    resp.sendRedirect("jsp/frame.jsp");
+                } else {
+                    // 没有对应的用户名和密码，返回登陆页
+                    req.setAttribute("error", "用户名或密码不正确");
+                    req.getRequestDispatcher("login.jsp").forward(req, resp);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 
