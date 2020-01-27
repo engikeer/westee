2926b1f58920488cde72cc2070b6cea848a1b077
engikeer <engikeer@github.com>
2020-01-28 00:53:48 +0800

    Update.

--- servlet-01/src/main/java/com/mfun/servlet/RelativePathServlet.java
+++ servlet-01/src/main/java/com/mfun/servlet/RelativePathServlet.java
@@ -9,11 +9,16 @@ import java.io.IOException;
 public class RelativePathServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String method = req.getParameter("method");
-        if ("redirect".equals(method)) {
-            resp.sendRedirect("hello");
-        } else {
-            req.getRequestDispatcher("hello").forward(req, resp);
-        }
+//        String method = req.getParameter("method");
+//        if ("redirect".equals(method)) {
+//            resp.sendRedirect("hello");
+//        } else {
+//            req.getRequestDispatcher("hello").forward(req, resp);
+//        }
+        resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().println("<h1>转发前的内容</h1>");
+//        req.getRequestDispatcher("hello").forward(req, resp);
+        req.getRequestDispatcher("hello").include(req, resp);
+        resp.getWriter().println("<h1>转发后的内容</h1>");
     }
 }
