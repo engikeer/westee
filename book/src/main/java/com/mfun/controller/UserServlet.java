574565264eb4e9d6d78cb87b5e33f7986b0666b4
engikeer <engikeer@github.com>
2020-01-31 01:26:59 +0800

    Update.

--- book/src/main/java/com/mfun/controller/UserServlet.java
+++ book/src/main/java/com/mfun/controller/UserServlet.java
@@ -3,21 +3,23 @@ package com.mfun.controller;
 import com.mfun.pojo.User;
 import com.mfun.service.user.UserService;
 import com.mfun.service.user.UserServiceImpl;
+import com.mfun.utils.ServletUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 
 public class UserServlet extends BaseServlet {
     private final UserService userService = new UserServiceImpl();
 
-    private void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
+    private void login(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         try {
-            User user = userService.login(new User(null, username, password, null));
+            User requestUser = ServletUtils.param2bean(req, User.class);
+            User user = userService.login(requestUser);
             if (user == null) {
                 // 登陆失败，返回登陆页面
                 req.setAttribute("msg", "用户名密码错误");
@@ -26,24 +28,30 @@ public class UserServlet extends BaseServlet {
                 // 登陆成功，返回成功页面
                 resp.sendRedirect(req.getContextPath() + "/pages/user/login_success.jsp");
             }
+        } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            e.printStackTrace();
+            resp.getWriter().println("请求参数值错误");
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
     }
 
-    private void logon(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-        String email = req.getParameter("email");
+    private void logon(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         try {
-            userService.logon(new User(null, username, password, email));
+            User user = ServletUtils.param2bean(req, User.class);
+            userService.logon(user);
             // 注册成功，前往成功页
             resp.sendRedirect(req.getContextPath() + "/pages/user/regist_success.jsp");
+        } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
+            e.printStackTrace();
+            resp.getWriter().println("请求参数值错误");
         } catch (SQLException e) {
             // 注册失败，返回注册页
             req.setAttribute("msg", "用户已存在");
             req.getRequestDispatcher("/pages/user/regist.jsp").forward(req, resp);
         }
+
     }
 }
---
+++ book/src/main/java/com/mfun/utils/ServletUtils.java
@@ -0,0 +1,58 @@
+package com.mfun.utils;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+public class ServletUtils {
+
+    /**
+     * 通过反射将请求中的参数封装为对象
+     * @param cls 封装对象的类对象
+     * @param <T> 封装对象的类型
+     * @return 封装对象
+     */
+    public static <T> T param2bean(HttpServletRequest req, Class<T> cls)
+            throws InvocationTargetException, IllegalAccessException, InstantiationException {
+        // 手动实现的过程
+        // 1. 获取类中声明的所有字段名
+//        Field[] fields = cls.getDeclaredFields();
+//        T t = cls.newInstance();
+//        // 2. 通过字符串获取所有的 setAttrName 方法
+//        for (Field field : fields) {
+//            String name = field.getName();
+//            // 通过字符取子串实现首字母大写
+//            // String methodName = "set" + name.substring(0, 1).toUpperCase() + name.substring(1);
+//            // 通过 ascii 码迁移实现首字母大写
+//            char[] chars = name.toCharArray();
+//            chars[0] -= 32;
+//            String methodName = "set" + String.valueOf(chars);
+//            Class<?> type = field.getType();
+//            // 3. 调用 setter 方法设置字段的值
+//            Method method = t.getClass().getMethod(methodName, type);
+//            method.invoke(t, req.getParameter(name));
+//        }
+
+        // 通过 beanutils 实现
+        // 存储属性时根据 setter 的名称设置，如果 name 与 setter 不对应则无法设置属性
+        // 该工具包能实现自动将传入的属性值转为字段所需的类型，如果无法转换会赋予默认值
+
+        // 通过 setProperty 实现
+//        Field[] fields = cls.getDeclaredFields();
+//        T t = cls.newInstance();
+//        for (Field field : fields) {
+//            String name = field.getName();
+//            BeanUtils.setProperty(t, name, req.getParameter(name));
+//        }
+
+        // 通过 populate 实现
+        T t = cls.newInstance();
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        BeanUtils.populate(t, parameterMap);
+        return t;
+    }
+}
